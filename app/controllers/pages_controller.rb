72022ef24db4e3fe55600c5bd9e484d44b5e588a
Michael N Brown <micha3lbrown@gmail.com>
2016-04-12 19:09:01 -0400

    Successfully retrieves, updates, and groups results.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
-require 'ruby-hackernews'
-
 class PagesController < ApplicationController
-  include RubyHackernews
 
   def home
-    byebug
-    Entry.all
   end
 
 end
---
+++ app/models/story.rb
@@ -0,0 +1,24 @@
+require 'ruby-hackernews'
+
+class Story < ActiveRecord::Base
+  include RubyHackernews
+
+  validates :external_id, presence: true
+
+  def self.save_entries_from_hn
+    entries = RubyHackernews::Entry.all(17).drop(10)
+    increment = Story.maximum(:active_group) + 1
+
+    stories = entries.map do |e|
+      s = Story.find_or_initialize_by(external_id: e.id, url: e.link.href)
+      s.external_id = e.id
+      s.title = e.link.title
+      s.url = e.link.href
+      s.active_group = increment
+      s.hn_score = e.voting.score
+      s.save
+      s
+    end
+    stories.select(&:persisted?)
+  end
+end
