2dc152283dd7be1684d9708ec4bdb887a4be2fdd
Michael N Brown <micha3lbrown@gmail.com>
2016-04-12 11:43:36 -0400

    basic auth complete

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20160412015641) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name",             limit: 255, default: "", null: false
+    t.string   "last_name",              limit: 255, default: "", null: false
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
