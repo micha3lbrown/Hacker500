48b6b0a2bfad2175e95494b20979e1a36a6ede6f
Michael N Brown <micha3lbrown@gmail.com>
2016-04-13 08:08:21 -0400

    comments incomplete, basic functinoality implemented

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413032117) do
+ActiveRecord::Schema.define(version: 20160413040335) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       limit: 65535,             null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160413032117) do
     t.integer  "story_id",   limit: 4,                 null: false
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "stories", force: :cascade do |t|
@@ -55,21 +56,6 @@ ActiveRecord::Schema.define(version: 20160413032117) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id",   limit: 4
-    t.string   "votable_type", limit: 255
-    t.integer  "voter_id",     limit: 4
-    t.string   "voter_type",   limit: 255
-    t.boolean  "vote_flag"
-    t.string   "vote_scope",   limit: 255
-    t.integer  "vote_weight",  limit: 4
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id",   limit: 4
